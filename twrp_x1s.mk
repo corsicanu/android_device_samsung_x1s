b9ccc1905d036c48e2d70c01408f02498378d520
corsicanu <me@corsicanu.ro>
2022-03-11 13:27:21 +0200

    tree: convert to x1s
    Signed-off-by: corsicanu <me@corsicanu.ro>

--- twrp_x1s.mk
+++ twrp_x1s.mk
@@ -5,19 +5,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/y2s/device.mk)
+$(call inherit-product, device/samsung/x1s/device.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/y2s/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/x1s/recovery/root,recovery/root)
 
 # Device identifier
-PRODUCT_RELEASE_NAME := y2s
-PRODUCT_DEVICE := y2s
-PRODUCT_NAME := twrp_y2s
+PRODUCT_RELEASE_NAME := x1s
+PRODUCT_DEVICE := x1s
+PRODUCT_NAME := twrp_x1s
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := Galaxy S20+
+PRODUCT_MODEL := Galaxy S20
 PRODUCT_MANUFACTURER := samsung
 
